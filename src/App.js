c1ea90c1216742567afd44b920e2c5cb52955b8c
hozuxx03 <106940992+hozuxx03@users.noreply.github.com>
2023-10-01 15:07:18 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./styles.css";
+
+export default function App() {
+  // map関数
+  const list = [1, 2, 3, 4];
+  const dataList = [
+    {
+      name: "taro",
+      age: 21
+    },
+    {
+      name: "ziro",
+      age: 32
+    }
+  ];
+  return (
+    <div className="App">
+      <h1>map-practice</h1>
+      {list.map((number) => (
+        <div>{number * 2}</div>
+      ))}
+      {dataList.map((data) => (
+        <div>
+          {data.name}
+          <span>{data.age}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
